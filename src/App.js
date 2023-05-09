6c2f5e1b56a353f907b04d50b6938e876077e1a9
Bahodir <rbaxa.br@gmail.com>
2023-05-09 20:05:01 +0500

    Changed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 
 import RMain from "./ResponsiveApp/RMain";
 import '../src/WeatherApp/Style.css'
-import { Offline, Online } from "react-detect-offline";
 
 function App() {
  
   return (
     <>
-    <Online>
     <div className="App">
       <RMain/>
     </div>
-    </Online>
-    <Offline>
       <h3>Error</h3>
-    </Offline>
     </>
   );
 }
